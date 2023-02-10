6f25d58d29028717f0c13111384716de12fe3160
Lucas <lvksounds@gmail.com>
2023-02-10 16:35:18 -0300

    .

---
+++ Web API Core/ConsoleToApi/ConsoleApp1/Controllers/AnimalsController.cs
@@ -0,0 +1,44 @@
+﻿using ConsoleToWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ConsoleToWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimalsController : ControllerBase
+    {
+        private List<AnimalModel> animals = null;
+        public AnimalsController()
+        {
+            animals = new List<AnimalModel>()
+            {
+                new AnimalModel() {Id = 1, Name = "Zulu"},
+                new AnimalModel() {Id = 2, Name = "Shawlin"}
+            };
+        }
+        
+        [Route("", Name = "All" )]
+        public IActionResult GetAnimals()
+        {
+            return Ok(animals);
+        }
+
+        [Route("test")]
+        public IActionResult GetAnimalsTest()
+        {
+            return AcceptedAtRoute("All");
+        }
+
+        [Route("{name}")]
+        public IActionResult GetAnimalsByName(string name)
+        {
+            if (!name.Contains("ABC"))
+            {
+                return BadRequest();
+            }
+            return Ok(name);
+        }
+    }
+}
